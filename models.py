6c5935edc8546a4393b1e009898ca9bab62122d6
Hyungrok Kim <q0115643@kaist.ac.kr>
2019-02-01 06:00:10 +0900

    merge brown_* and coca_* to one

--- models.py
+++ models.py
@@ -57,7 +57,7 @@ class Markov:
         state = self.init_chain()
         self.alphabets = alphabets
         self.states[state] = self.empty_counter()
-        self.alphabet_size = len(alphabets) + 1
+        self.alphabet_size = len(alphabets) + 1 # for <END>
         
     def init_chain(self):
         """Helper function to generate the correct initial chain value."""
@@ -96,13 +96,16 @@ class Markov:
         if len(prefix) > 0:
             for c in prefix:
                 request = self.step(request, c)
-        char_counter = self.states[request]
+        if request in self.states:
+            char_counter = self.states[request]
+        else:
+            char_counter = self.empty_counter()
         char_probs = []
         total_cnt = 0
         for tok, cnt in char_counter.items():
             total_cnt += cnt
         for tok, cnt in char_counter.items():
-            prob = (1. + float(cnt))/(float(total_cnt) + self.alphabet_size)
+            prob = (1. + float(cnt))/(float(total_cnt) + self.alphabet_size) # smoothing
             char_probs.append(tuple([tok, prob]))
         return char_probs
 
@@ -110,7 +113,7 @@ class Markov:
 class RNN_Map():
     def __init__(self, model_path, num_node, using_GPU, hidden_dim=64, alphabets=list(string.ascii_lowercase)):
         self.alphabets = alphabets
-        self.alphabet_size = len(alphabets) + 2
+        self.alphabet_size = len(alphabets) + 2 # for <PAD> and <END>
         self.int2char = dict(enumerate(alphabets, start=2))
         self.int2char[0] = '<PAD>'
         self.int2char[1] = '<END>'
@@ -124,7 +127,7 @@ class RNN_Map():
         self.RNN_model.eval()
         self.num_node = num_node
     
-    def get_pos(self, token):
+    def encode(self, token):
         assert len(token) > 0
         hidden = self.RNN_model.init_hidden()
         pos = 0
@@ -151,7 +154,7 @@ class RNN_Map():
                     else:
                         pos += prob * sec_len
                 else:
-                    pad_prob += prob
+                    pad_prob = prob # <PAD> prob merges into <END> prob
         with torch.no_grad():
             prev = Variable(prev)
             if self.using_GPU:
@@ -163,7 +166,7 @@ class RNN_Map():
     
     def get_node(self, token):
         sec_len = 1. / self.num_node
-        pos = self.get_pos(token)
+        pos = self.encode(token)
         return 1 + int(pos // sec_len)
     
     def cnt_per_node(self, num_node, test_tokens):
@@ -172,6 +175,44 @@ class RNN_Map():
             node_num = self.get_node(token)
             cnt_per_node[node_num - 1] += 1
         return cnt_per_node
+    
+    def decode(self, pos):
+        token = ''
+        sec_len = 1
+        std = 0
+        hidden = self.RNN_model.init_hidden()
+        prev = torch.stack([torch.Tensor(np.zeros((1, self.alphabet_size)))])
+        while True:
+            char_probs = []
+            with torch.no_grad():
+                prev = Variable(prev)
+                if self.using_GPU:
+                    prev = prev.cuda()
+                    hidden = (hidden[0].cuda(), hidden[1].cuda())
+                output, hidden = self.RNN_model.forward2(prev, hidden)
+                pad_prob = 0
+                for idx, prob in enumerate(list(output.cpu().numpy()[0][0])):
+                    if idx == 0:
+                        pad_prob = prob # <PAD> prob merges into <END> prob
+                        continue
+                    elif idx == 1:
+                        prob += pad_prob
+                    char_probs.append(tuple([self.int2char[idx], prob]))
+            for tok, prob in char_probs:
+                std += sec_len*prob
+                if pos < std:
+                    tok = tok
+                    std -= sec_len*prob
+                    sec_len *= prob
+                    break
+            if tok == "<END>":
+                break
+            token += tok
+            prev = [torch.Tensor(np.zeros((1, self.alphabet_size)))]
+            prev[0][0][self.char2int[tok]] = 1
+            prev = torch.stack(prev)
+        return token
+
 
 class Markov_Map():
     def __init__(self, order, train_tokens, num_node, alphabets=list(string.ascii_lowercase)):
@@ -179,7 +220,7 @@ class Markov_Map():
         self.num_node = num_node
         self.markov.learn(train_tokens)
 
-    def get_pos(self, token):
+    def encode(self, token):
         assert len(token) > 0
         pos = 0
         sec_len = 1
@@ -194,7 +235,7 @@ class Markov_Map():
             for tok, cnt in output.items():
                 total_cnt += cnt
             for tok, cnt in output.items():
-                prob = (1. + float(cnt))/(float(total_cnt) + self.markov.alphabet_size)
+                prob = (1. + float(cnt))/(float(total_cnt) + self.markov.alphabet_size) # smoothing
                 char_probs.append(tuple([tok, prob]))
             for tok, prob in char_probs:
                 if tok == c:
@@ -218,7 +259,7 @@ class Markov_Map():
 
     def get_node(self, token):
         sec_len = 1. / self.num_node
-        pos = self.get_pos(token)
+        pos = self.encode(token)
         return 1 + int(pos // sec_len)
 
     def cnt_per_node(self, num_node, test_tokens):
@@ -227,3 +268,23 @@ class Markov_Map():
             node_num = self.get_node(token)
             cnt_per_node[node_num - 1] += 1
         return cnt_per_node
+    
+    def decode(self, pos):
+        token = ''
+        sec_len = 1
+        std = 0
+        prev = self.markov.init_chain()
+        while True:
+            char_probs = self.markov.get_probs(prev)
+            for tok, prob in char_probs:
+                std += sec_len*prob
+                if pos < std:
+                    tok = tok
+                    std -= sec_len*prob
+                    sec_len *= prob
+                    break
+            if tok == "<END>":
+                break
+            token += tok
+            prev = self.markov.step(prev, tok)
+        return token
---
+++ parser.py
@@ -0,0 +1,98 @@
+from nltk.tokenize import word_tokenize
+from tqdm import tqdm
+from util import list_to_string, only_alphabets
+import csv
+import sys
+import logging
+from sklearn.model_selection import train_test_split
+import argparse
+from collections import Counter
+import re
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--corpus', type=int, default='coca',
+                       help='if set brown, does not use gram_num')
+    parser.add_argument('--gram_num', type=int, default=2)
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m-%d %H:%M', stream=sys.stdout)
+
+if args.corpus = 'brown':
+    brown_fp = './data/corpora/brown.txt'
+    train_tokens_fp = './data/brown/train_tokens.txt'
+    val_tokens_fp = './data/brown/val_tokens.txt'
+    test_tokens_fp = './data/brown/test_tokens.txt'
+    tokens = []
+    logging.info('Tokenizing Brown Corpus...')
+    with open(brown_fp, 'r') as brown:
+        brown = list(brown)
+        for line in tqdm(brown, total=len(brown)):
+            words = word_tokenize(only_alphabets(line))
+            tokens += words
+    logging.info("Number of Tokens: {}".format(len(tokens)))
+    logging.info("Number of Different Tokens: {}".format(len(list(set(tokens)))))
+    train_tokens, test_tokens = train_test_split(tokens, test_size=0.1)
+    train_tokens, val_tokens = train_test_split(train_tokens, test_size=0.05)
+    logging.info("Writing Train Tokens info on new txt file...")
+    with open(train_tokens_fp, 'w') as writeFile:
+        train_tokens = sorted(list(set(train_tokens)), key = lambda s : s.lower())
+        for token in tqdm(train_tokens):
+            writeFile.write("%s\n" % token)
+    logging.info("Writing Validation Tokens info on new txt file...")
+    with open(val_tokens_fp, 'w') as writeFile:
+        val_tokens = sorted(list(set(val_tokens)), key = lambda s : s.lower())
+        for token in tqdm(val_tokens):
+            writeFile.write("%s\n" % token)
+    logging.info("Writing Test Tokens info on new txt file...")
+    with open(test_tokens_fp, 'w') as writeFile:
+        test_tokens = sorted(list(set(test_tokens)), key = lambda s : s.lower())
+        for token in tqdm(test_tokens):
+            writeFile.write("%s\n" % token)
+elif args.corpus = 'coca':
+    gram_num = args.gram_num
+    coca_fp = './data/corpora/coca_' + str(gram_num) + 'gram.txt'
+    train_tokens_fp = './data/coca/' + str(gram_num) + 'gram/train_tokens.txt'
+    val_tokens_fp = './data/coca/' + str(gram_num) + 'gram/val_tokens.txt'
+    test_tokens_fp = './data/coca/' + str(gram_num) + 'gram/test_tokens.txt'
+    tokens = []
+    logging.info('Tokenizing Brown Corpus...')
+    with open(coca_fp, 'r') as coca:
+        coca = list(coca)
+        for line in tqdm(coca, total=len(coca)):
+            items = [item.strip() for item in line.split('\t')]
+            freq = int(items[0])
+            if freq > 1:
+                token = ''
+                for i in range(gram_num):
+                    if i != 0:
+                        token += '_'
+                    token += re.sub(r"\s+", "", only_alphabets(items[i+1]))
+                for _ in range(freq-1):
+                    tokens.append(token)
+    logging.info("Number of Tokens: {}".format(len(tokens)))
+    logging.info("Number of Different Tokens: {}".format(len(list(set(tokens)))))
+    train_tokens, test_tokens = train_test_split(tokens, test_size=0.1)
+    train_tokens, val_tokens = train_test_split(train_tokens, test_size=0.1)
+    train_tokens = list(set(train_tokens))
+    val_tokens = list(set(val_tokens))
+    test_tokens = list(set(test_tokens))
+    logging.info("Writing Training Tokens info on new txt file...")
+    with open(train_tokens_fp, 'w') as writeFile:
+        train_tokens = sorted(train_tokens, key = lambda s : s.lower())
+        for token in tqdm(train_tokens):
+            writeFile.write("%s\n" % token)
+    logging.info("Writing Validation Tokens info on new txt file...")
+    with open(val_tokens_fp, 'w') as writeFile:
+        val_tokens = sorted(val_tokens, key = lambda s : s.lower())
+        for token in tqdm(val_tokens):
+            writeFile.write("%s\n" % token)
+    logging.info("Writing Test Tokens info on new txt file...")
+    with open(test_tokens_fp, 'w') as writeFile:
+        test_tokens = sorted(test_tokens, key = lambda s : s.lower())
+        for token in tqdm(test_tokens):
+            writeFile.write("%s\n" % token)
